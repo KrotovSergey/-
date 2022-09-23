3ca417b0f666e5f9c87eff55ab1dbdb1a9445f8c
Sergey <serega_krotov90@mail.ru>
2022-09-23 22:03:21 +0500

    zadacha13_dz2

---
+++ Homework_2_zadacha_13/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int a = int.Parse(Console.ReadLine());
+
+if (a>100)
+{
+    int b = (a % 10);
+    Console.WriteLine(b);
+}
+else 
+{
+   Console.WriteLine("Число двузначное");
+}
+        
