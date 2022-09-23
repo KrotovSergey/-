f495af3bd9875ace795d8075919d25dff66f3c24
Sergey <serega_krotov90@mail.ru>
2022-09-23 22:13:17 +0500

    zadacha15_dz2

---
+++ Homework_2_zadacha_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 6) 
+{
+Console.WriteLine("Будний день");
+}
+else if (number >5 && number < 8)
+{
+Console.WriteLine("Этот день выходной, ура");
+} 
+else if (number > 7)
+{
+Console.WriteLine("Введите число от 1 до 7");
+} 
