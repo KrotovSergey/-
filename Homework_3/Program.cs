1f5137c823a51878f71a92a7106ed63d746162e9
Sergey <serega_krotov90@mail.ru>
2022-09-26 22:27:58 +0500

    zadacha19

---
+++ Homework_3/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+Console.WriteLine("Какую задачу проверить?: 19/21/23 ");
+
+int zadacha = int.Parse(Console.ReadLine());
+
+if (zadacha == 19) {
+    Zadacha1();
+    }
+
+
+void Zadacha1(){
+    Console.Clear();
+
+    Console.WriteLine("Введите 5-ти значное число:");
+
+    int num = int.Parse(Console.ReadLine());
+
+    int a = num/10000;
+    
+    int b = num%10;
+
+    int c = (num - (num/10000)*10000);
+  
+    int d = (c - (c%10))/10;
+
+
+    int e  = d/100;
+
+    int f = d%10;
+  
+    if(a == b && e == f) {
+        Console.WriteLine("это число является палиндромом");
+    }
+    else
+            {
+                Console.WriteLine("это число не является палиндромом");
+            }
+} 
+
+void Zadacha2()
